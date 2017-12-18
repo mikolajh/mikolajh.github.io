1e2b6f156b91385572d39bc0063dc0e395527622
mikolajh <test@test.com>
2017-12-18 18:53:59 +0200

    portfolio

---
+++ portfolio/js/script.js
@@ -0,0 +1,16 @@
+//$('.sl').slick({
+ // infinite: true,
+  //slidesToShow: 2,
+  //slidesToScroll: 1,
+  //autoplay: true,
+  //autoplaySpeed: 3000,
+  //dots: true,
+  // fade: true - коли один слайд тоді змінюється краще
+//});
+
+$(document).ready(function(){
+  $('.sl').slick({
+    setting-name: setting-value
+  });
+});
+		
